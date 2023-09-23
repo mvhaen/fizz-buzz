11aebe56fa74d754054241d7a97040f3edb54ab2
Michael Voorhaen <michael.voorhaen@gmail.com>
2023-09-23 13:09:20 +0200

    first failing test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", fizz_buzz(1));
+}
+
+fn fizz_buzz(n: i32) -> String {
+    return "".to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test] 
+    fn it_returns_1_for_1() {
+        assert_eq!("1", fizz_buzz(1));
+    }
 }
