3714b342a74f4e1dbdad8d6701c2c764ebcb62a6
Michael Voorhaen <michael.voorhaen@gmail.com>
2023-09-23 15:00:16 +0200

    fizz for 5

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ fn main() {
 }
 
 fn fizz_buzz(n: i32) -> String {
+    if n % 15 == 0 {
+        return "Fizz-Buzz".to_string();
+    }
     if n % 3 == 0 {
         return "Fizz".to_string();
-    } else if n % 5 == 0 {
-        return "buzz".to_string();
+    }
+    if n % 5 == 0 {
+        return "Buzz".to_string();
     }
     n.to_string()
 }
@@ -36,7 +40,29 @@ mod tests {
     }
 
     #[test]
-    fn it_returns_fizz_for_5() {
-        assert_eq!("buzz", fizz_buzz(5));
+    fn it_returns_buzz_for_5() {
+        assert_eq!("Buzz", fizz_buzz(5));
     }
+
+    #[test]
+    fn it_returns_fizz_for_6() {
+        assert_eq!("Fizz", fizz_buzz(6));
+    }
+
+    #[test]
+    fn it_returns_7_for_7() {
+        assert_eq!("7", fizz_buzz(7));
+    }
+
+    // array with numbers divisible by 3 and 5 up to 100
+    const FIZZ_BUZZ: [i32; 6] = [15, 30, 45, 60, 75, 90];
+
+    // parameterized test
+    #[test]
+    fn it_returns_fizz_buzz_for_15_30_45_60_75_90() {
+        for n in FIZZ_BUZZ.iter() {
+            assert_eq!("Fizz-Buzz", fizz_buzz(*n));
+        }
+    }
+
 }
